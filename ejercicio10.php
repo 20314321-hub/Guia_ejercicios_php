04986e7e330b181c1815f0a49735f630d808618d
20314321-hub <20314321@clases.edu.sv>
2026-05-20 08:51:59 -0600

    ejercicio10
    10. Ejercicio 10
    Diseñe un formulario que solicite tres notas de un estudiante y envíe la información a otro archivo PHP donde se calcule y muestre el promedio.

---
+++ ejercicio10.php
@@ -0,0 +1,10 @@
+<?php
+$nota1 = isset($_POST['nota1']) ? floatval($_POST['nota1']) : 0;
+$nota2 = isset($_POST['nota2']) ? floatval($_POST['nota2']) : 0;
+$nota3 = isset($_POST['nota3']) ? floatval($_POST['nota3']) : 0;
+
+$promedio = ($nota1 + $nota2 + $nota3) / 3;
+
+echo "<h1>";
+echo "El promedio es: " . $promedio;
+?>
