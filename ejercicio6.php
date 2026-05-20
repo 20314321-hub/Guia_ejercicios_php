058a2fd3cb521606ae3c217b53d77afffa6d6cd1
20314321-hub <20314321@clases.edu.sv>
2026-05-20 08:49:26 -0600

    ejercicio6
    6. Ejercicio 6
    Elabore un formulario que solicite el nombre y la edad de una persona y envíe la información a otro archivo PHP donde se muestren ambos datos.

---
+++ ejercicio6.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST["apellido"];
+$edad = $_POST['edad'];
+echo "bicho tu nombre es: " . $nombre;
+echo "<br>";
+echo "bicho tu apellido es: " . $apellido;
+echo"<br>";
+echo "bicho ya estas viejo, tienes: " . $edad;
+?>
